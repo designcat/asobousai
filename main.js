e6f7e43957a92fd154c86698eab684c652d6cc0d
Shinnosuke Komiya <kurohune538@gmail.com>
2016-07-21 17:20:14 +0900

    add sp css & font scalable

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+var windowWidth;
 function initialize() {
   var marker;
   var latlng = new google.maps.LatLng(34.677300,138.945937);
@@ -37,5 +38,43 @@ function initialize() {
   var sampleType = new google.maps.StyledMapType(styleOptions, styledMapOptions);
   map.mapTypes.set('sample', sampleType);
   map.setMapTypeId('sample');
+
+  //ウインドウサイズを取得
+  getWidowSize();
+
+  //イベントやブースのタイトルのサイズ変更
+  changeFontSize();
 }
 google.maps.event.addDomListener(window, 'load', initialize);
+window.onresize = getWidowSize;
+
+function getWidowSize() {
+  windowWidth = window.innerWidth;
+  console.log(windowWidth);
+  changeFontSize();
+}
+
+function changeFontSize() {
+  var textBoxWidth = (windowWidth *  3 / 10 * 0.8) - 20;
+  console.log("changeFontSize");
+  var className = document.getElementsByClassName('contents__title');
+  for (j=0; j<className.length; j++) {
+    console.log("classは全部で"+className.length+"あります");
+    console.log(j+"の字の幅は"+ className[j].offsetWidth + "px");
+    console.log("枠の幅は" + textBoxWidth + "px");
+    className[j].style.fontSize = 2.2 + "rem";
+    if (textBoxWidth <= (className[j].offsetWidth)){
+      for(i=2.2; i>=1; i-=0.1){
+        className[j].style.fontSize = i + "rem";
+        console.log(j + "のフォントサイズは" + i + "rem");
+        console.log(j+"の字の幅は"+ className[j].offsetWidth + "pxその2");
+        if (textBoxWidth >= className[j].offsetWidth) {
+          console.log(j + "は枠の幅以下になりました");
+          break;
+        }
+      }
+    } else {
+      console.log("幅より小さいです");
+    }
+  }
+}
